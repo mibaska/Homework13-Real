a153f1af7877ae20c369cf34fccb421110bdbba0
Michael Baska <mikebaska1@gmail.com>
2020-04-17 14:28:14 -0500

    put posts back

--- routes/api.js
+++ routes/api.js
@@ -1,53 +1,53 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
-router.put("/api/transaction/bulk", (req, res) => {
-  Transaction.updateOne(
-    { _id: req.params.id },
-    { name: req.params.name },
-    { value: req.params.value }
-    )
-  .then(dbTransaction => {
-    res.json(dbTransaction);
-  })
-  .catch(err => {
-    res.status(400).json(err);
-  });
-});
-
-// router.put("/api/transaction", ({ body }, res) => {
-//   Transaction.create(body)
-//     .then(dbTransaction => {
-//       res.json(dbTransaction);
-//     })
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
+// router.put("/api/transaction/:id", (req, res) => {
+//   Transaction.updateOne(
+//     { _id: req.params.id },
+//     { name: req.params.name },
+//     { value: req.params.value }
+//     )
+//   .then(dbTransaction => {
+//     res.json(dbTransaction);
+//   })
+//   .catch(err => {
+//     res.status(400).json(err);
+//   });
 // });
 
-router.put("/api/transaction/bulk", (req, res) => {
-  Transaction.updateMany(
-    { _id: req.params.id },
-    { name: req.params.name },
-    { value: req.params.value }
-    )
-  .then(dbTransaction => {
-    res.json(dbTransaction);
-  })
-  .catch(err => {
-    res.status(400).json(err);
-  });
+router.post("/api/transaction", ({ body }, res) => {
+  Transaction.create(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
 });
 
-// router.put("/api/transaction/bulk", ({ body }, res) => {
-//   Transaction.insertMany(body)
-//     .then(dbTransaction => {
-//       res.json(dbTransaction);
-//     })
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
+// router.put("/api/transaction/bulk", (req, res) => {
+//   Transaction.updateMany(
+//     { _id: req.params.id },
+//     { name: req.params.name },
+//     { value: req.params.value }
+//     )
+//   .then(dbTransaction => {
+//     res.json(dbTransaction);
+//   })
+//   .catch(err => {
+//     res.status(400).json(err);
+//   });
 // });
+
+router.post("/api/transaction/bulk", ({ body }, res) => {
+  Transaction.insertMany(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
 
 router.get("/api/transaction", (req, res) => {
   Transaction.find({})
