516cdcc7fcfd9f50ca31282d7b58e3eeebbba019
Michael Baska <mikebaska1@gmail.com>
2020-04-17 14:17:02 -0500

    please

--- public/index.js
+++ public/index.js
@@ -112,7 +112,7 @@ function sendTransaction(isAdding) {
 
   // also send to server
   fetch("/api/transaction", {
-    method: "POST",
+    method: "PUT",
     body: JSON.stringify(transaction),
     headers: {
       Accept: "application/json, text/plain, */*",
