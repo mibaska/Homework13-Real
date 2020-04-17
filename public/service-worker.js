e00954c9635c44b60f6cbd57bcdc88c41dda50dd
Michael Baska <mikebaska1@gmail.com>
2020-04-17 12:17:00 -0500

    post didn't work because put

--- public/service-worker.js
+++ public/service-worker.js
@@ -41,7 +41,7 @@ self.addEventListener("fetch", event => {
 
         return caches.open(RUNTIME).then(cache => {
           return fetch(event.request).then(response => {
-            return cache.put(event.request, response.clone()).then(() => {
+            return cache.post(event.request, response.clone()).then(() => {
               return response;
             });
           });
