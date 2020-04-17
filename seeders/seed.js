0acbe8286f19ca0bac678564864ec6aa41e871fa
Michael Baska <mikebaska1@gmail.com>
2020-04-17 15:55:35 -0500

    fuckingWHYgit add -A!

--- seeders/seed.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-var db = require('../models');
-
-//make the connection to the db using mongoose
-mongoose.connect(process.env.MONGODB.URI || 'mongodb://localhost/budget', {
-    useNewUrlParser: true
-});
